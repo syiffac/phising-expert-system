bf7591c44f3437edca3e9306a467b772ff0b23b3
syiffac <syifaasyikaa@gmail.com>
2026-06-29 01:34:10 +0700

    Update CORS for custom domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,6 +40,7 @@ if not origins:
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://phising-expert-system.vercel.app",
+        "https://www.phishguards.web.id"
     ]
 
 app.add_middleware(
